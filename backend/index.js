e73f3fbb3aefa28cbacd538e20429282e6b1f08a
Nishita <nishi77600@gmail.com>
2026-03-06 21:23:31 +0530

    My local changes before merge

--- backend/index.js
+++ backend/index.js
@@ -6,49 +6,32 @@ const app = express();
 
 console.log("INDEX FILE RUNNING");
 
-// middleware
 app.use(express.json());
 
-// routes
-const authRoutes = require("./routes/authRoutes");
 const tokenRoutes = require("./routes/tokenRoutes");
-
-app.use("/api/auth", authRoutes);
 app.use("/api/token", tokenRoutes);
 
-// root route (ye server.js me tha)
-app.get("/", (req, res) => {
-    res.send("Smart Token System Running ✅");
-});
-
-// test route
-app.get("/test", (req, res) => {
-    console.log("TEST ROUTE HIT");
-    res.send("Server working");
-});
-
-// create http server
 const server = http.createServer(app);
 
-// socket setup
-const io = new Server(server, {
-    cors: {
-        origin: "*"
+const io = new Server(server,{
+    cors:{
+        origin:"*"
     }
 });
 
-// middleware to access io in controllers
-app.use((req, res, next) => {
+app.use((req,res,next)=>{
     req.io = io;
     next();
 });
 
-// socket connection
-io.on("connection", (socket) => {
-    console.log("User connected:", socket.id);
+io.on("connection",(socket)=>{
+    console.log("User connected:",socket.id);
 });
 
-// start server
-server.listen(3000, () => {
+app.get("/test",(req,res)=>{
+    res.send("Server working");
+});
+
+server.listen(3000,()=>{
     console.log("Server running on port 3000");
 });
